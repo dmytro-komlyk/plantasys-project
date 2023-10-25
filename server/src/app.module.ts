55cb6e23dc6482f8767b3d1384ff72a51556c7d6
Dmitriy <dima.komlyk@gmail.com>
2023-10-25 17:03:10 +0300

    feat: implemented mongo database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_DB_URI'),
+        dbName: config.get<string>('MONGO_DB_NAME'),
+        auth: {
+          username: config.get<string>('MONGO_DB_AUTH_USERNAME'),
+          password: config.get<string>('MONGO_DB_AUTH_PASSWORD'),
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
